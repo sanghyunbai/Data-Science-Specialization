a154961631a69bd3e0d49754ea79c46151b96e9c
sanghyun <sanghyun@kth.se>
2019-02-16 21:10:40 +0100

    part3; checking (if state is valid & given output is valid) is implemented

--- module2_R_Programming/Week4_Programming_Assignment/main.R
+++ module2_R_Programming/Week4_Programming_Assignment/main.R
@@ -78,7 +78,10 @@ run <-function(){
         # print(paste("Result  ",best("MD", "pneumonia",outcome),sep = ":"))
         
         # Part3; Rank hospitals 
-        # source(filepath("rankhospital",dirPath(),"R"))
+        source(filepath("rankhospital",dirPath(),"R"))
+        #test 0
         rankhospital("TX","heart failure",4,outcome)
+        # rankhospital("TX1","heart failure",4,outcome)
+        rankhospital("TX","heart failure1",4,outcome)
 }
 run()
--- module2_R_Programming/Week4_Programming_Assignment/rankhospital.R
+++ module2_R_Programming/Week4_Programming_Assignment/rankhospital.R
@@ -5,11 +5,22 @@
 ## excluded from the set of hospitals when deciding the rankings.
 ## multiple hospitals have the same 30-day mortality rate for a given causeof death. 
 ## In those cases ties should be broken by using the hospital name
-rankhospital <- function(state,specified_outcome,num ="best",data){
+rankhospital <- function(specified_state,specified_outcome,num ="best",data){
         ## state = name of state
         ## specified_out come = result caused by c(heart attack,heart failure,pneumonia)
         ## num = can be “best”, “worst”,  or an integer 
         ## indicating the ranking(smaller numbers are better)
         ## data = dataset
-        print("in?")
+        
+        library(dplyr)
+        #check if state is valid 
+        if(!is.element(specified_state,data$State)){
+                stop("invalid state")
+        }
+        #check if specified_outcome is valid 
+        if (!is.element(specified_outcome,c("heart attack", "heart failure", "pneumonia"))) {
+                stop('invalid outcome')
+        }
+        
+        
 }
