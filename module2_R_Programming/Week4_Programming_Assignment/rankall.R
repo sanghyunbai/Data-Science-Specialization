9ba96bd9f6631bf4f3c857a6f5b1e64212bcc67e
sanghyun <sanghyun@kth.se>
2019-02-17 00:14:29 +0100

    Part4; Ranking hospitals in all states. rankall.R file is created, sourcing rankall.R from main.R is tested

---
+++ module2_R_Programming/Week4_Programming_Assignment/rankall.R
@@ -0,0 +1,13 @@
+## The function reads theoutcome-of-care-measures.csvfile 
+## and returns a 2-column data framecontaining the hospital in each state 
+## that has the ranking specified in num.  
+## For example the function callrankall("heart attack", "best")
+## would return a data frame containing the names of the hospitals 
+## that are the best in their respective states for 30-day heart attack death rates.  
+## The function should return a valuefor every state (some may be NA)
+## Handling ties.  Therankallfunction should handle ties in the 30-day mortality rates 
+## in the same way that therankhospitalfunction handles ties.
+
+rankall <- function(specified_outcome, num="best"){
+        print("in?")
+}
